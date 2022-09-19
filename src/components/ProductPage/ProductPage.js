e579b318dc0b4539c8e8e492eb197fdf055d4b61
Francksuel <francksuelbarbosa@gmail.com>
2022-09-18 22:19:14 -0300

    Adding in cart's user

--- src/components/ProductPage/ProductPage.js
+++ src/components/ProductPage/ProductPage.js
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getProductApi } from "../../service/VintageSoulService.js";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+	getProductApi,
+	getToken,
+	postCartApi,
+} from "../../service/VintageSoulService.js";
 import styled from "styled-components";
 import Header from "../Header/Header.js";
 import ProductInfo from "./ProductInfo.js";
 import WrapperButton from "../styled-components/WrapperButton.js";
 
 export default function ProductPage() {
+	const navigate = useNavigate();
 	const { idProduct } = useParams();
 	const [product, setProduct] = useState({});
 	const [quantity, setQuantity] = useState(1);
@@ -14,12 +19,46 @@ export default function ProductPage() {
 		getProductApi({ idProduct })
 			.then((res) => {
 				setProduct(res.data);
-				console.log(res.data);
 			})
 			.catch((res) => {
 				alert(res.response.data);
 			});
 	}, [idProduct]);
+
+	function increaseQuantity() {
+		const newValue = quantity + 1;
+		setQuantity(newValue);
+	}
+	function decreaseQuantity() {
+		const newValue = quantity - 1;
+		setQuantity(newValue);
+	}
+
+	function addToCart(e) {
+		e.preventDefault();
+		const isLogged = getToken();
+		if (!isLogged) {
+			alert("Você não está logado!");
+			navigate("/sign-in");
+		}
+		const request = postCartApi({
+			image: product.image,
+			title: product.title,
+			price: product.price,
+			idProduct: product._id,
+			quantity: quantity			
+		});
+		request.then(() => {
+			alert("Produto adicionado ao carrinho");
+			navigate("/");
+		});
+		request.catch((error) => {
+			if (error.code === "ERR_NETWORK") {
+				return alert("Falha ao conectar com o servidor");
+			}
+			alert(error.response.data);
+		});
+	}
 
 	return (
 		<>
@@ -29,19 +68,21 @@ export default function ProductPage() {
 			) : (
 				<Wrapper>
 					<ProductInfo product={product} />
-					<QuantityControl>
-						<ButtonAdd>-</ButtonAdd>
-						<input
-							type="number"
-							min="1"
-							max={product.inventory}
-							value={quantity}
-							onChange={(e) => setQuantity(e.target.value)}
-							required
-						></input>
-						<ButtonAdd onClick={() => alert("oi")}>+</ButtonAdd>						
-					</QuantityControl>
-					<WrapperButton value={"Adicionar ao carrinho"}/>
+					<form onSubmit={addToCart}>
+						<QuantityControl>
+							<ButtonAdd onClick={() => decreaseQuantity()}>-</ButtonAdd>
+							<input
+								type="number"
+								min="1"
+								max={product.inventory}
+								value={quantity}
+								onChange={(e) => setQuantity(e.target.value)}
+								required
+							></input>
+							<ButtonAdd onClick={() => increaseQuantity()}>+</ButtonAdd>
+						</QuantityControl>
+						<WrapperButton value={"Adicionar ao carrinho"} />
+					</form>
 				</Wrapper>
 			)}
 		</>
@@ -55,6 +96,13 @@ const Wrapper = styled.div`
 	align-items: center;
 	margin: 0 auto;
 	max-width: 600px;
+	form {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+	}
 `;
 const ButtonAdd = styled.div`
 	display: flex;
@@ -66,7 +114,7 @@ const ButtonAdd = styled.div`
 	color: white;
 	font-size: 15px;
 	font-weight: 700;
-	border-radius: 3px;	
+	border-radius: 3px;
 `;
 const QuantityControl = styled.div`
 	display: flex;
